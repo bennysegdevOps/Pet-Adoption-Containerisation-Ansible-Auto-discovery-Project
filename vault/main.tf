e66b27bd78995380fa86bcf0262c0fa4e654ebbc
olusegun-odede <olusegun.odede@cloudhight.com>
2023-06-01 22:03:30 -0500

    vault update

---
+++ vault/main.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region    = var.region
+  profile   = var.profile
+}
+
+#EC2 keypair
+resource "aws_key_pair" "vault-keypair" {
+  key_name = "vault-keypair"
+  public_key = var.public-key
+}
+
+#Security group for Vault
+resource "aws_security_group" "Vault-SG" {
+  name          = "Vault-SG"
+  description   = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "http access"
+    from_port        = var.port_http
+    to_port          = var.port_http
+    protocol         = var.sg-protocol
+    cidr_blocks      = [var.all_cidr]
+  }
+
+  ingress {
+    description      = "https access"
+    from_port        = var.port_https
+    to_port          = var.port_https
+    protocol         = var.sg-protocol
+    cidr_blocks      = [var.all_cidr]
+  }
+
+    ingress {
+    description      = "vault access"
+    from_port        = var.port_vault
+    to_port          = var.port_vault
+    protocol         = var.sg-protocol
+    cidr_blocks      = [var.all_cidr]
+  }
+
+    ingress {
+    description      = "ssh access"
+    from_port        = var.port_ssh
+    to_port          = var.port_ssh
+    protocol         = var.sg-protocol
+    cidr_blocks      = [var.all_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [var.all_cidr]
+  }
+
+  tags = {
+    Name = "Vault-SG"
+  }
+}
+
+#EC2 for Terraform Vault
+resource "aws_instance" "vault" {
+  ami                       = var.vault-ami
+  instance_type             = var.instance_type
+  vpc_security_group_ids    = [aws_security_group.Vault-SG.id]
+  key_name                  = aws_key_pair.vault-keypair.key_name
+  iam_instance_profile      = aws_iam_instance_profile.vault-kms-unseal.id
+  associate_public_ip_address = true
+  user_data                 = local.vault_user_data
+  
+  tags = {
+    Name = "${local.name}-vault-server"
+  }
+}
+
+# KMS key
+resource "aws_kms_key" "vault" {
+  description             = "vault unseal key"
+  deletion_window_in_days = 10
+
+  tags = {
+    Name = "Vault-KMS-unseal"
+  }
+}
+
+# Route 53 and Record 
+data "aws_route53_zone" "vault" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "vault_record" {
+  zone_id = data.aws_route53_zone.vault.zone_id
+  name    = var.domain_name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.vault.public_ip]
+}
---
+++ vault/vault-script.tf
@@ -0,0 +1,87 @@
+locals {
+  vault_user_data = <<-EOF
+#!/bin/bash
+sudo apt update
+sudo wget https://releases.hashicorp.com/consul/1.7.3/consul_1.7.3_linux_amd64.zip
+sudo apt install unzip -y
+sudo unzip consul_1.7.3_linux_amd64.zip
+sudo mv consul /usr/bin/
+sudo cat <<EOT>> /etc/systemd/system/consul.service
+[Unit]
+Description=Consul 
+Documentation=https://www.consul.io/
+
+[Service]
+ExecStart=/usr/bin/consul agent -server -ui -data-dir=/temp/consul -bootstrap-expect=1 -node=vault -bind=$(hostname -i) -config-dir=/etc/consul.d/ 
+ExecReload=/bin/kill -HUP $MAINPID 
+LimitNOFILE=65536 
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+sudo mkdir /etc/consul.d
+sudo cat <<EOT>> /etc/consul.d/ui.json
+{
+    "addresses":{
+    "http": "0.0.0.0"
+    }
+}
+EOT
+
+sudo systemctl daemon-reload
+sudo systemctl start consul
+sudo systemctl enable consul
+sudo apt update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo apt-get install certbot -y
+sudo certbot certonly --standalone -d teemone.xyz --email amanzeedward@yahoo.com --agree-tos --non-interactive
+sudo wget https://releases.hashicorp.com/vault/1.5.0/vault_1.5.0_linux_amd64.zip
+sudo unzip vault_1.5.0_linux_amd64.zip
+sudo mv vault /usr/bin/
+sudo mkdir /etc/vault/
+sudo cat <<EOT>> /etc/vault/config.hcl
+storage "consul" {
+        address = "127.0.0.1:8500"
+        path ="vault/"
+}
+listener "tcp"{
+          address = "0.0.0.0:443"
+          tls_disable = 0
+          tls_cert_file = "/etc/letsencrypt/live/teemone.xyz/fullchain.pem"
+          tls_key_file = "/etc/letsencrypt/live/teemone.xyz/privkey.pem"
+}
+seal "awskms" {
+  region     = "${var.aws_region}"
+  kms_key_id = "${aws_kms_key.vault.id}"
+}
+ui = true
+EOT
+
+sudo cat <<EOT>> /etc/systemd/system/vault.service
+[Unit]
+Description=Vault
+Documentation=https://www.vault.io/
+
+[Service]
+ExecStart=/usr/bin/vault server -config=/etc/vault/config.hcl
+ExecReload=/bin/kill -HUP $MAINPID
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+sudo systemctl daemon-reload
+export VAULT_ADDR="https://teemone.xyz:443"
+cat << EOT > /etc/profile.d/vault.sh
+export VAULT_ADDR="https://teemone.xyz:443"
+export VAULT_SKIP_VERIFY=true
+EOT
+vault -autocomplete-install
+complete -C /usr/bin/vault vault
+sudo systemctl start vault
+sudo systemctl enable vault
+EOF
+}
