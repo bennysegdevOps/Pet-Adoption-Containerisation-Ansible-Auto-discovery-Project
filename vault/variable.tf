708d2f218acab853172f759ec5f200f5d0d19a2c
bennysegdevOps <128261749+bennysegdevOps@users.noreply.github.com>
2023-09-02 16:06:42 -0500

    Update variable.tf

--- vault/variable.tf
+++ vault/variable.tf
@@ -1,52 +1,48 @@
 variable "region" {
   default = "eu-west-1"
 }
-
 variable "profile" {
   default = "default"
 }
-
 variable "public-key" {
   default = "~/Keypairs/vault-key.pub"
 }
-
 variable "all_cidr" {
   default = "0.0.0.0/0"
 }
-
 variable "port_ssh" {
   default = "22"
 }
-
-# proxy port for vault
 variable "port_vault" {
   default = "8200"
 }
-
 variable "port_http" {
   default = "80"
 }
-
 variable "port_https" {
   default = "443"
 }
-
 variable "sg-protocol" {
   default = "tcp"
 }
-
 variable "vault-ami" {
   default = "ami-01dd271720c1ba44f"
 }
-
 variable "instance_type" {
   default = "t2.medium"
 }
-
 variable "domain_name" {
   default = "wehabot.com"
 }
-
 variable "aws_region" {
   default = "eu-west-1"
+}
+variable "email" {
+  default = ""
+}
+variable "api_key" {
+  default = ""
+}
+variable "account_id" {
+  default = ""
 }
